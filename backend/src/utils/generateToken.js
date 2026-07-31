62cafa1f28106e8ecfc437564b39e253d112dcc7
VISHAL952840 <vishalchaudhary952840@gmail.com>
2026-07-31 14:40:38 +0530

    Add logout function

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -1,11 +1,20 @@
+// import jwt from "jsonwebtoken";
+
+// const generateToken = (userId) => {
+//     return jwt.sign({ id: userId },
+//         process.env.JWT_SECRET_KEY, {
+//             expiresIn: "7d"
+//         }
+//     );
+// };
+
+// export default generateToken;
 import jwt from "jsonwebtoken";
 
-const generateToken = (userId) => {
-    return jwt.sign({ id: userId },
-        process.env.JWT_SECRET_KEY, {
-            expiresIn: "7d"
-        }
-    );
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "30d",
+  });
 };
 
 export default generateToken;
