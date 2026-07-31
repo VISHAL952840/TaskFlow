3d2994345aedc8c6934327e6fb63d78b8b3a92fe
VISHAL952840 <vishalchaudhary952840@gmail.com>
2026-07-31 14:31:30 +0530

    Add logout function

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -7,55 +7,7 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-//     const login = async (email, password) => {
-//         const response = await api.post("/auth/login", {
-//             email,
-//             password,
-//         });
 
-//         localStorage.setItem("token", response.data.token);
-//         setUser(response.data);
-
-//         return response.data;
-//     };
-
-//     const register = async (name, email, password) => {
-//         const response = await api.post("/api/auth/register", {
-//     name,
-//     email,
-//     password,
-// });
-
-//         localStorage.setItem("token", response.data.token);
-//         setUser(response.data);
-
-//         return response.data;
-//     };
-
-//     const logout = () => {
-//         localStorage.removeItem("token");
-//         setUser(null);
-//     };
-
-//     const getCurrentUser = async () => {
-//         try {
-//             const token = localStorage.getItem("token");
-
-//             if (!token) {
-//                 setLoading(false);
-//                 return;
-//             }
-
-//             const response = await api.get("/auth/me");
-
-//             setUser(response.data);
-//         } catch (error) {
-//             localStorage.removeItem("token");
-//             setUser(null);
-//         } finally {
-//             setLoading(false);
-//         }
-//     };
 const login = async (email, password) => {
     const response = await api.post("/api/auth/login", {
         email,
@@ -79,6 +31,10 @@ const register = async (name, email, password) => {
     setUser(response.data);
 
     return response.data;
+};
+const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
 };
 
 const getCurrentUser = async () => {
